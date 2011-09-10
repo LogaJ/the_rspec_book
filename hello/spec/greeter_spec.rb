9da55c04a0c4ce2d3e1e468d6f2dee353d952824
loga jegede <loga.jegede@gmail.com>
2011-09-10 12:01:00 +0100

    fixing the hello specs load path to avoid using './' in the require

--- hello/spec/greeter_spec.rb
+++ hello/spec/greeter_spec.rb
@@ -1,4 +1,5 @@
-require './rspec_greeter'
+$LOAD_PATH << File.dirname(__FILE__)
+require 'rspec_greeter'
 
 describe 'RSpec Greeter' do
   it "should say 'Hello RSpec!' when it receives the greet() message" do
